7a565a4738e2073587653e02e78da5bd8712935e
Черба <sasha-cherba@yandex.ru>
2022-05-29 19:41:17 +0300

    зачет-2

--- main.py
+++ main.py
@@ -1,78 +1,82 @@
-import faker
+from faker import Faker
 import conf
 import random
-import sys
+import json
 
 
+fake = Faker(locale="ru_RU")
 
 
 def title():
-    f = open("book.txt","r",encoding='utf-8')
-    Book_lines = ""
-    Book_line = f.read()
+    with open("book.txt","r",encoding='utf-8') as f:
+            Book_lines = ""
+            Book_line = f.read()
     S = Book_line.split("\n")
     Title = random.choice(S)
     return Title
 def year():
-    return random.randint()
+    return random.randint(1,2020)
 def pages():
-    return random.randint()
+    return random.randint(50,2000)
 def rating():
-    return random.random(1,5)
+    return random.randint(1,5)
 def price():
     return random.random()
 def author():
     L = []
-    for i in random.random(1,2,3):
-        name = faker.name
+    for i in range(1,3):
+        name = fake.name()
         L.append(name)
 
 
 
 
-def dict_generator(pk_d = 1):
+def dict_generator(pk_d = 1, n = 1):
 
     pk = pk_d
-    model = conf.model
-    Title = ""
-    Title = title()
-    year = 0
-    pages = 0
-    rating = 0.0
-    year = year()
-    pages = pages()
-    isbn13 = faker.isbn13
-    rating = rating()
-    price = 0.0
-    price = price()
-    author = []
-    author = author()
-    Dict = {}
-    Dict = {
-        "model" : model,
-        "pk" : pk,
-        "fields" : {
-            "title" : title,
-            "year" : year,
-            "pages" : pages,
-            "isbn13" : isbn13,
-            "rating" : rating,
-            "price" : price,
-            "author" : author
+    for i in range(n):
+            Dict = {
+                "model" : conf.model,
+                "pk" : pk,
+                "fields" : {
+                    "title" : title(),
+                    "year" : year(),
+                    "pages" : pages(),
+                    "isbn13" : fake.isbn13(),
+                    "rating" : rating(),
+                    "price" : price(),
+                    "author" : author()
 
-        }
-    }
-    pk += 1
-    yield Dict
+                }
+            }
+            pk += 1
+
+            yield Dict
 
 
 
 
 if __name__ == '__main__':
-    g = dict_generator()
+
+    g = dict_generator(n=250)
+
     L = []
-    for i in range(1,100):
-      L.append(next(g))
+    for i in range(1,250):
+          D = next(g)
+          L.append(D)
+
+    with open("dict_file.json", "w") as file:
+        json.dump(L,file)
+
+    with open("dict_file.json", "r") as file:
+        LL = json.load(file)
+        print(type(LL))
+    for line in LL:
+        print(line)
+
+
+
+
 
 
 
